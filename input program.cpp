4f9dbb0ebb6de182aa1501023189519f9d41403a
hafsacore <110922795+hafsacore@users.noreply.github.com>
2023-01-25 10:50:26 +0500

    Add files via upload

---
+++ input program.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,sum;
+
+    cout<<"Enter your first number: \n"<<a;
+    
+    cout<<"Enter your second number: \n"<<b;
+    
+    cout<<"The sum of given numbers is: \n"<<a+b;
+    return 0;
+
+} 
